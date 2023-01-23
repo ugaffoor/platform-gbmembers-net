85d1454b82c29d6fe69b8c997def8ebbc32ffa10
Unus Gaffoor <unus.gaffoor@kineticdata.com>
2023-01-23 14:08:27 +0800

    emailAttachment size limit set

--- runOZrb.rb
+++ runOZrb.rb
@@ -1,10 +1,10 @@
 require "json"
 
-space_name = "gbmembers-template.kinops.io"
+space_name = "gbmembers-template.gbmembers.net"
 space_slug = "gbmembers-template"
-server = "https://gbmembers-template.kinops.io"
+server = "https://gbmembers-template.gbmembers.net"
 username = "unus@uniqconsulting.com.au"
-password = "gbfms@2017"
+password = "gbfms@2021"
 
 data = {
 "core" => {
@@ -16,8 +16,8 @@ data = {
   "space_name" => space_name,
   "service_user_username" => username,
   "service_user_password" => password,
-  "task_api_v1" => "#{server}/app/components/task/app/api/v1",
-  "task_api_v2" => "#{server}/app/components/task/app/api/v2",
+  "task_api_v1" => "https://manage.gbmembers.net/app/components/task/app/api/v1",
+  "task_api_v2" => "https://manage.gbmembers.net/app/components/task/app/api/v2",
   },
   "http_options" => {
     "log_level" => "info",
